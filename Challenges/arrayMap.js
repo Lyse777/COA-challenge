e3dd9c501656ca4dbd5f267b7ea2774fb1aa4842
Lyse777 <umuhirelise22@gmail.com>
2024-06-08 00:13:15 +0200

    challenges

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,24 @@
+function findSubarrayWithSum(arr, target) {
+    let left = 0;
+    let currentSum = 0;
+    
+    for (let right = 0; right < arr.length; right++) {
+        currentSum += arr[right];
+        
+        while (currentSum > target && left <= right) {
+            currentSum -= arr[left];
+            left++;
+        }
+        
+        if (currentSum === target) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+
+const arr = [4, 2, 7, 1, 9, 5];
+const target = 17;
+console.log(findSubarrayWithSum(arr, target));  
